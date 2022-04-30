fbada8ee85a31c3f0933a537900fb10c0df19506
Layson <Layson-Lee@xxxxx.com>
2022-04-30 22:27:49 +0800

    test-1.2

--- src/com/layson/Hello.java
+++ src/com/layson/Hello.java
@@ -5,5 +5,7 @@ public class Hello {
         System.out.println("hello world 1");
         System.out.println("hello world 2");
         System.out.println("hello world 3");
+
+        System.out.println("hot-fix");
     }
 }

3f98e1c4415478d014c63278462a15a4869a503e
Layson <Layson-Lee@xxxxx.com>
2022-04-30 22:24:46 +0800

    test-1.0

---
+++ src/com/layson/Hello.java
@@ -0,0 +1,9 @@
+package com.layson;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello world 1");
+        System.out.println("hello world 2");
+        System.out.println("hello world 3");
+    }
+}
